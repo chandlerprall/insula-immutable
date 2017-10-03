d0a5bc740145de4646834144364f7abc24c387cd
Chandler Prall <chandler.prall@gmail.com>
2017-10-02 22:05:43 -0600

    Console.warn when a non-immutable object (or child of an immutable object) is passed; resolves #1

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+var HAS_PROCESS_ENV_NODE_ENV = typeof process !== 'undefined' && Object.prototype.hasOwnProperty(process, 'env') && Object.prototype.hasOwnProperty(process.env, 'NODE_ENV');
+var IS_PRODUCTION = HAS_PROCESS_ENV_NODE_ENV && process.env.NODE_ENV === 'production';
+var Immutable = require('immutable');
+
+function isImmutable(value) {
+    // immutable v4 added `isImmutable`
+    if (Immutable.hasOwnProperty(isImmutable)) {
+        return Immutable.isImmutable(value);
+    } else {
+        return Immutable.Map.isMap(value) ||
+            Immutable.List.isList(value) ||
+            Immutable.Stack.isStack(value) ||
+            Immutable.Set.isSet(value) ||
+            Immutable.OrderedMap.isOrderedMap(value) ||
+            Immutable.OrderedSet.isOrderedSet(value);
+    }
+}
+
+function verifyDeeplyImmutable(value, selector) {
+    var valueType = typeof value;
+    switch (valueType) {
+        case 'string':
+        case 'boolean':
+        case 'number':
+        case 'null':
+        case 'undefined':
+            // these primatives are fine
+            break;
+        default:
+            if (isImmutable(value)) {
+                // at a top level everything is great, look at the values
+                var childValues = value.values();
+                for (var i = 0; i < childValues.length; i++) {
+                    verifyDeeplyImmutable(childValues[i], selector);
+                }
+            } else {
+                console.warn('insula-immutable was called with a non-immutable object or child for selector ' + JSON.stringify(selector) + ' and value', value);
+            }
+    }
+}
+
 var InsulaImmutableMiddleware = {
     constructor: function insulaImmutableConstructor() {
         this.accessStateAtSelector = function insulaImmutableAccessStateAtSelector(selector) {
@@ -5,6 +46,10 @@ var InsulaImmutableMiddleware = {
         };
         
         this.setStateAtSelector = function insulaImmutableSetStateAtSelector(selector, value) {
+            if (!IS_PRODUCTION) {
+                verifyDeeplyImmutable(value, selector);
+            }
+
             if (selector.length === 0) {
                 this.setState(value);
             } else {
